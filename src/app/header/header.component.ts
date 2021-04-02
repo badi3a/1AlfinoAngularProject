846438433990229846169f194218f92b8f03d8b9
badi3a <badiaabouhdid@gmail.com>
2021-04-02 16:44:40 +0200

    séance 3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,7 +19,7 @@ export class HeaderComponent implements OnInit {
     this.title = '1Alinfo6 Project';
     this.list = [
       {id: 12, title: 'T-shirt 1', price: 10, quantity: 50, like: 3},
-      {id: 13, title: 'T-shirt 2', price: 340, quantity: 2, like: 0},
+      {id: 13, title: 'T-shirt 2', price: 340, quantity: 0, like: 0},
       {id: 14, title: 'T-shirt 3', price: 34, quantity: 1, like: 2}
     ];
   }
@@ -38,5 +38,12 @@ export class HeaderComponent implements OnInit {
 
   buyProduct(p: Product) {
     p.quantity--;
+  }
+  getClass(p: Product) {
+    if (p.quantity === 0) {
+      return 'btn btn-danger';
+    } else {
+      return 'btn btn-success';
+    }
   }
 }
