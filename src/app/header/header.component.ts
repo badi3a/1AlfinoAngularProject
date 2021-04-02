afea4d4e5a80eecca022372f3b4683777e42c096
badi3a <badiaabouhdid@gmail.com>
2021-04-02 15:39:36 +0200

    séance 3

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../model/product';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+
+export class HeaderComponent implements OnInit {
+  title: string;
+  list: Product[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.title = '1Alinfo6 Project';
+    this.list = [
+      {id: 12, title: 'T-shirt 1', price: 10, quantity: 50, like: 3},
+      {id: 13, title: 'T-shirt 2', price: 34, quantity: 2, like: 0},
+      {id: 14, title: 'T-shirt 3', price: 34, quantity: 1, like: 2},
+      {id: 15, title: 'T-shirt 4', price: 34, quantity: 0, like: 20},
+    ];
+  }
+
+  incrementLikeByProduct(p: Product) {
+    p.like++;
+  }
+
+  getValue(p: Product) {
+    if (p.quantity === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  buyProduct(p: Product) {
+    p.quantity--;
+  }
+}
