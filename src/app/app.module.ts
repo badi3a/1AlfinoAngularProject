cd044f97af81f3ff9282e8b4e58d7961b66942d3
badi3a <badiaabouhdid@gmail.com>
2021-06-18 18:01:34 +0200

    Seance 7.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {FormsModule} from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { FavoritClassroomComponent } from './favorit-classroom/favorit-classroom.component';
-import { ClassroomComponent } from './classroom/classroom.component';
+import {HeaderComponent} from './header/header.component';
+import {FavoritClassroomComponent} from './favorit-classroom/favorit-classroom.component';
+import {ClassroomComponent} from './classroom/classroom.component';
+import {AddClassroomComponent} from './add-classroom/add-classroom.component';
+import {Routes, RouterModule} from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,13 +17,17 @@ import { ClassroomComponent } from './classroom/classroom.component';
     HomeComponent,
     HeaderComponent,
     FavoritClassroomComponent,
-    ClassroomComponent
+    ClassroomComponent,
+    AddClassroomComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
